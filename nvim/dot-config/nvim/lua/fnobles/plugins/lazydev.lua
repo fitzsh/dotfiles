0141a4e112121954afd608dae969ce0f62a8be0e
Fitzpatrick Nobles <hi@fitz.sh>
2025-10-27 15:44:23 -0600

    Cleaned up lazydev.lua

--- nvim/dot-config/nvim/lua/fnobles/plugins/lazydev.lua
+++ nvim/dot-config/nvim/lua/fnobles/plugins/lazydev.lua
@@ -4,21 +4,9 @@ return {
 		ft = "lua", -- only load on lua files
 		opts = {
 			library = {
-				-- See the configuration section for more details
-				-- Load luvit types when the `vim.uv` word is found
-				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				"~/.config/nvim",
 			},
 		},
-	},
-	{ -- optional cmp completion source for require statements and module annotations
-		"hrsh7th/nvim-cmp",
-		opts = function(_, opts)
-			opts.sources = opts.sources or {}
-			table.insert(opts.sources, {
-				name = "lazydev",
-				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-			})
-		end,
 	},
 	{ -- optional blink completion source for require statements and module annotations
 		"saghen/blink.cmp",
@@ -31,12 +19,9 @@ return {
 					lazydev = {
 						name = "LazyDev",
 						module = "lazydev.integrations.blink",
-						-- make lazydev completions top priority (see `:h blink.cmp`)
-						score_offset = 100,
 					},
 				},
 			},
 		},
 	},
-	-- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
 }
