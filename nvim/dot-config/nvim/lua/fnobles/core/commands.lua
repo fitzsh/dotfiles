d71204a3454894956cbd166346f712a50ef8f257
Fitzpatrick Nobles <hi@fitz.sh>
2025-11-21 10:31:09 -0700

    New command file, added commands to use with the Snacks.picker

---
+++ nvim/dot-config/nvim/lua/fnobles/core/commands.lua
@@ -0,0 +1,14 @@
+--[[
+ ██████╗ ██████╗ ███╗   ███╗███╗   ███╗ █████╗ ███╗   ██╗██████╗ ███████╗
+██╔════╝██╔═══██╗████╗ ████║████╗ ████║██╔══██╗████╗  ██║██╔══██╗██╔════╝
+██║     ██║   ██║██╔████╔██║██╔████╔██║███████║██╔██╗ ██║██║  ██║███████╗
+██║     ██║   ██║██║╚██╔╝██║██║╚██╔╝██║██╔══██║██║╚██╗██║██║  ██║╚════██║
+╚██████╗╚██████╔╝██║ ╚═╝ ██║██║ ╚═╝ ██║██║  ██║██║ ╚████║██████╔╝███████║
+ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝
+--]]
+
+-- snacks picker
+vim.cmd([[command! -nargs=0 GoToCommand :lua Snacks.picker.command_history()]])
+vim.cmd([[command! -nargs=0 GoToFile :lua Snacks.picker.smart()]])
+vim.cmd([[command! -nargs=0 GoToSymbol :lua Snacks.picker.lsp_symbols()]])
+vim.cmd([[command! -nargs=0 Grep :lua Snacks.picker.grep()]])
