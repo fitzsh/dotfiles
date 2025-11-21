db87694eaa822e434575cdc5fa80c50dae2b04ed
Fitzpatrick Nobles <hi@fitz.sh>
2025-11-21 10:20:04 -0700

    Updated blink plugin to use normal, not mono, for nerd font

--- nvim/dot-config/nvim/lua/fnobles/plugins/blink.lua
+++ nvim/dot-config/nvim/lua/fnobles/plugins/blink.lua
@@ -1,14 +1,36 @@
 return {
 	{
 		"saghen/blink.cmp",
-		dependencies = { "rafamadriz/friendly-snippets" },
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"MahanRahmati/blink-nerdfont.nvim",
+		},
 		version = "1.*",
 		opts = {
-			keymap = { preset = "default" },
+			keymap = {
+				preset = "default",
+			},
 			appearance = {
-				nerd_font_variant = "mono",
+				nerd_font_variant = "normal",
+			},
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer", "nerdfont" },
+				providers = {
+					nerdfont = {
+						module = "blink-nerdfont",
+						name = "Nerd Fonts",
+						score_offset = 15, -- Tune by preference
+						opts = { insert = true }, -- Insert nerdfont icon (default) or complete its name
+					},
+				},
+			},
+			completion = {
+				accept = { auto_brackets = { enabled = true } },
+				documentation = { auto_show = true },
+				list = { selection = { preselect = true } },
 			},
 		},
 		signature = { enabled = true },
+		opts_extend = { "sources.default" },
 	},
 }
