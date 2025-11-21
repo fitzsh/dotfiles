8dbffa26e3cd2fc5e4885a15ced1787ae26c57d7
Fitzpatrick Nobles <hi@fitz.sh>
2025-11-21 10:30:24 -0700

    New file for auto commands, added command to highlight text when yanking

---
+++ nvim/dot-config/nvim/lua/fnobles/core/auto_commands.lua
@@ -0,0 +1,17 @@
+--[[
+ █████╗ ██╗   ██╗████████╗ ██████╗  ██████╗███╗   ███╗██████╗ ███████╗
+██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗██╔════╝████╗ ████║██╔══██╗██╔════╝
+███████║██║   ██║   ██║   ██║   ██║██║     ██╔████╔██║██║  ██║███████╗
+██╔══██║██║   ██║   ██║   ██║   ██║██║     ██║╚██╔╝██║██║  ██║╚════██║
+██║  ██║╚██████╔╝   ██║   ╚██████╔╝╚██████╗██║ ╚═╝ ██║██████╔╝███████║
+╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝  ╚═════╝╚═╝     ╚═╝╚═════╝ ╚══════╝
+See `:help lua-guide-autocommands`
+--]]
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
